5b85b8b337c0f392ac72993424a76c5fcb3b00f3
muhamar <52808054+amarpr@users.noreply.github.com>
2019-08-13 17:08:16 +0700

    formedit.php
    menambahkan file formedit.php

---
+++ formedit.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>form edit</title>
+  </head>
+  <body>
+    <h1><u>FORM EDIT</u></h1> <br>
+    <form>
+  <div class="form-group">
+    <label>Kode Buku</label>
+    <input class="form-control" placeholder="input update kode buku">
+  </div>
+  <div class="form-group">
+    <label>Judul Buku</label>
+    <input class="form-control" placeholder="input update judul buku">
+  </div>
+  <div class="form-group">
+    <label>Pengarang</label>
+    <input class="form-control" placeholder="input update pengarang">
+  </div>
+  <div class="form-group">
+    <label>Penerbit</label>
+    <input class="form-control" placeholder="input update penerbit">
+  </div>
+  <div class="form-group">
+    <label>Tahun Terbit</label>
+    <input class="form-control" placeholder="input update tahun terbit">
+  </div>
+
+  <button class="btn btn-primary">Save</button>
+</form>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
